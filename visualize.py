fbd2fab14b9fea0fb0d591055da29fb3069f580a
sakar kharel <97268953+sakarkharel@users.noreply.github.com>
2025-12-31 11:50:50 +0545

    refactor: code and csv update

--- visualize.py
+++ visualize.py
@@ -20,19 +20,11 @@ company = st.sidebar.selectbox("Choose a company", companies)
 df = load_data(company)
 
 st.subheader(f"Weekly Data for {company}")
-st.write(df.tail())  
+st.write(df.head())  
 
 st.subheader(f"{company} Close Price Trend")
 st.line_chart(df.set_index('Date')['Close'])
 
-
-# st.subheader(f"{company} Weekly Volume")
-# fig, ax = plt.subplots()
-# ax.bar(df['Date'], df['Volume'], color='orange')
-# ax.set_xticklabels(df['Date'], rotation=90)
-# ax.set_xlabel('Date')
-# ax.set_ylabel('Volume')
-# st.pyplot(fig)
 
 st.subheader(f"{company} Candlestick Chart")
 fig = go.Figure(data=[go.Candlestick(
@@ -55,4 +47,3 @@ for c in companies:
 
 comparison_df = pd.DataFrame(all_data)
 st.line_chart(comparison_df)
-
